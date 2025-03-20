30a611ad5053a783150b4bf6cf12aba2ce5e5c87
amkikoti <tonykikoti@gmail.com>
2025-03-20 15:23:22 +0300

    first commit

---
+++ src/main/java/com/kikoti/bajetiapp/BajetiappApplication.java
@@ -0,0 +1,13 @@
+package com.kikoti.bajetiapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BajetiappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BajetiappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kikoti/bajetiapp/BajetiappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kikoti.bajetiapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BajetiappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
